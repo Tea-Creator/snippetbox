6fe492c82ba4a3c5c66f954b064858c3ec97117d
Asylkhan Azat <asylkhan.azat@gmail.com>
2021-01-18 01:26:33 +0600

    chore(configuration): Replaced hard-coded values via flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
 )
 
-const (
-	// Port on witch server will start listening incoming requests
-	Port = 4000
-)
-
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
@@ -22,7 +21,7 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on :%d\n", Port)
+	log.Printf("Starting server on %s\n", *addr)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Port), mux))
+	log.Fatal(http.ListenAndServe(*addr, mux))
 }
