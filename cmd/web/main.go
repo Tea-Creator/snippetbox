960aeebc7ed5b427d2ff06bcc5c47f349050c4c1
Asylkhan Azat <asylkhan.azat@gmail.com>
2021-01-20 04:13:49 +0600

    chore(db): Added dbpool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func main() {
@@ -14,11 +17,13 @@ func main() {
 type startupConfig struct {
 	Address     string
 	StaticFiles string
+	ConnString  string
 }
 
 func (conf *startupConfig) setup() {
 	flag.StringVar(&conf.Address, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&conf.StaticFiles, "static_files", "./ui/static/", "Directory with static content")
+	flag.StringVar(&conf.ConnString, "conn_string", "", "Database Connection String")
 	flag.Parse()
 }
 
@@ -46,6 +51,14 @@ func (a *app) run() {
 	a.errorLogger = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	a.configureRoutes()
+
+	pool, err := pgxpool.Connect(context.Background(), a.conf.ConnString)
+
+	if err != nil {
+		a.errorLogger.Fatal(err.Error())
+	}
+
+	defer pool.Close()
 
 	srv := &http.Server{
 		Addr:     a.conf.Address,
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Tea-Creator/snippetbox
 
 go 1.15
 
-require github.com/jackc/pgx/v4 v4.10.1 // indirect
+require (
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jackc/pgx/v4 v4.10.1
+)
